f0bcb70d620bb6515b4262d83dc29e85a5bd7df0
Oskar Köök <oskarkook@maatriks.ee>
2018-08-02 14:55:00 +0300

    Add injection for AppSwitcher

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 const windowSwitcherPopup = imports.ui.altTab.WindowSwitcherPopup;
+const appSwitcher = imports.ui.altTab.AppSwitcher;
 
 let injections = {};
 
@@ -11,8 +12,16 @@ function enable() {
     let allWindows = injections['_getWindowList'].call(this);
     return allWindows.filter(w => w.get_monitor() === global.screen.get_current_monitor());
   }
+  
+  injections['_addIcon'] = appSwitcher.prototype._addIcon;
+  appSwitcher.prototype._addIcon = function(icon) {
+    icon.cachedWindows = icon.cachedWindows.filter(w => w.get_monitor() === global.screen.get_current_monitor());
+    if(icon.cachedWindows.length === 0) return;
+    return injections['_addIcon'].call(this, icon);
+  }
 }
 
 function disable() {
   windowSwitcherPopup.prototype._getWindowList = injections['_getWindowList'];
+  appSwitcher.prototype._addIcon = injections['_addIcon'];
 }
