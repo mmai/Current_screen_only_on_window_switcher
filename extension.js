227a43154bb571d927207e077a6384ed7c8bed2a
Henri Bourcereau <henri.bourcereau@gmail.com>
2018-10-24 11:25:12 +0200

    gnome 3.30 / ubuntu 18.10 compatibility

--- extension.js
+++ extension.js
@@ -9,7 +9,7 @@ function enable() {
   injections['_getWindowList'] = windowSwitcherPopup.prototype._getWindowList;
   windowSwitcherPopup.prototype._getWindowList = function() {
     let allWindows = injections['_getWindowList'].call(this);
-    return allWindows.filter(w => w.get_monitor() === global.screen.get_current_monitor());
+    return allWindows.filter(w => w.get_monitor() === global.display.get_current_monitor());
   }
 }
 
