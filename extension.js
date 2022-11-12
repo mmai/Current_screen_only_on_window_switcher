1ff6484f53bddb5c163b217a2d57d4ef61ff04dc
Henri Bourcereau <henri.bourcereau@gmail.com>
2022-11-12 11:24:42 +0100

    remove gnome < 3.32 code

--- extension.js
+++ extension.js
@@ -1,3 +1,5 @@
+//  See the `gnome3` branch for a version compatible with gnome < 3.32
+
 const { GObject } = imports.gi;
 
 const PACKAGE_VERSION = imports.misc.config.PACKAGE_VERSION;
@@ -10,38 +12,12 @@ let CurrentMonitorAppSwitcher;
 
 let injections = {};
 
-function init() {
-  if (PACKAGE_VERSION >= '3.32') {
-    init_3_32();
-  }
-}
-
-function enable() {
-  if (cmpVersions(PACKAGE_VERSION, '3.32') < 0){
-    enable_older();
-  } else {
-    enable_3_32();
-  }
-}
-
-function disable() {
-  if (cmpVersions(PACKAGE_VERSION, '3.32') < 0){
-    disable_older();
-  } else {
-    disable_3_32();
-  }
-}
-
-/*******************************
- *   gnome-shell >= 3.32       *
- *******************************/
-
 function getCurrentMonitorWindows() {
     let allWindows = injections['getWindows'].apply(this, arguments);
-    return allWindows.filter(w => w.get_monitor() === getCurrentMonitor());
+    return allWindows.filter(w => w.get_monitor() === global.display.get_current_monitor());
 }
 
-function init_3_32() {
+function init() {
   CurrentMonitorWindowSwitcherPopup = GObject.registerClass(
     class CurrentMonitorWindowSwitcherPopup extends altTab.WindowSwitcherPopup {
       vfunc_allocate() {
@@ -67,7 +43,7 @@ function init_3_32() {
   CurrentMonitorAppSwitcher = GObject.registerClass(
     class CurrentMonitorAppSwitcher extends altTab.AppSwitcher {
       _addIcon(icon) {
-        icon.cachedWindows = icon.cachedWindows.filter(w => w.get_monitor() === getCurrentMonitor());
+        icon.cachedWindows = icon.cachedWindows.filter(w => w.get_monitor() === global.display.get_current_monitor());
         if(icon.cachedWindows.length === 0) {
           return;
         }
@@ -76,7 +52,7 @@ function init_3_32() {
   });
 }
 
-function enable_3_32() {
+function enable() {
   injections['getWindows'] = altTab.getWindows;
   altTab.getWindows = getCurrentMonitorWindows;
 
@@ -90,92 +66,9 @@ function enable_3_32() {
   altTab.AppSwitcher = CurrentMonitorAppSwitcher;
 }
 
-function disable_3_32(){
+function disable(){
   altTab.getWindows = injections['getWindows'];
   altTab.WindowSwitcherPopup = injections['WindowSwitcherPopup'];
   altTab.AppSwitcherPopup = injections['appSwitcherPopup'];
   altTab.AppSwitcher = injections['appSwitcher'];
-}
-
-/*******************************
- *   gnome-shell < 3.32        *
- *******************************/
-
-function enable_older() {
-  injections['_getWindowList'] = altTab.WindowSwitcherPopup.prototype._getWindowList;
-  altTab.WindowSwitcherPopup.prototype._getWindowList = function() {
-    let allWindows = injections['_getWindowList'].apply(this, arguments);
-    return allWindows.filter(w => w.get_monitor() === getCurrentMonitor());
-  }
-
-  injections['_getPreferredWidth'] = altTab.WindowSwitcherPopup.prototype._getPreferredWidth;
-  altTab.WindowSwitcherPopup.prototype._getPreferredWidth = function() {
-    let originalPrimaryMonitor = main.layoutManager.primaryMonitor;
-    main.layoutManager.primaryMonitor = main.layoutManager.currentMonitor;
-    let response = injections['_getPreferredWidth'].apply(this, arguments);
-    main.layoutManager.primaryMonitor = originalPrimaryMonitor;
-    return response;
-  }
-
-  injections['_getPreferredHeight'] = altTab.WindowSwitcherPopup.prototype._getPreferredHeight;
-  altTab.WindowSwitcherPopup.prototype._getPreferredHeight = function() {
-    let originalPrimaryMonitor = main.layoutManager.primaryMonitor;
-    main.layoutManager.primaryMonitor = main.layoutManager.currentMonitor;
-    let response = injections['_getPreferredHeight'].apply(this, arguments);
-    main.layoutManager.primaryMonitor = originalPrimaryMonitor;
-    return response;
-  }
-
-  injections['_allocate'] = altTab.WindowSwitcherPopup.prototype._allocate;
-  altTab.WindowSwitcherPopup.prototype._allocate = function() {
-    let originalPrimaryMonitor = main.layoutManager.primaryMonitor;
-    main.layoutManager.primaryMonitor = main.layoutManager.currentMonitor;
-    let response = injections['_allocate'].apply(this, arguments);
-    main.layoutManager.primaryMonitor = originalPrimaryMonitor;
-    return response;
-  }
-
-  injections['_addIcon'] = altTab.AppSwitcher.prototype._addIcon;
-  altTab.AppSwitcher.prototype._addIcon = function(icon) {
-    icon.cachedWindows = icon.cachedWindows.filter(w => w.get_monitor() === getCurrentMonitor());
-    if(icon.cachedWindows.length === 0) {
-      return;
-    }
-    return injections['_addIcon'].call(this, icon);
-  }
-}
-
-function disable_older(){
-  altTab.WindowSwitcherPopup.prototype._getWindowList = injections['_getWindowList'];
-  altTab.WindowSwitcherPopup.prototype._getPreferredWidth = injections['_getPreferredWidth'];
-  altTab.WindowSwitcherPopup.prototype._getPreferredHeight = injections['_getPreferredHeight'];
-  altTab.WindowSwitcherPopup.prototype._allocate = injections['_allocate'];
-  altTab.AppSwitcher.prototype._addIcon = injections['_addIcon'];
-}
-
-/*******************************
- *           utils             *
- *******************************/
-
-function getCurrentMonitor(){
-  if (cmpVersions(PACKAGE_VERSION, '3.30') < 0){
-    return global.screen.get_current_monitor();
-  }
-  return global.display.get_current_monitor();
-}
-
-function cmpVersions (a, b) {
-    var i, diff;
-    var regExStrip0 = /(\.0+)+$/;
-    var segmentsA = a.replace(regExStrip0, '').split('.');
-    var segmentsB = b.replace(regExStrip0, '').split('.');
-    var l = Math.min(segmentsA.length, segmentsB.length);
-
-    for (i = 0; i < l; i++) {
-        diff = parseInt(segmentsA[i], 10) - parseInt(segmentsB[i], 10);
-        if (diff) {
-            return diff;
-        }
-    }
-    return segmentsA.length - segmentsB.length;
 }
